c4defb3847b8ca2ec4fca6693deab3f6497eb3f0
unknown <cindy2001920@naver.com>
2022-10-11 12:57:57 +0900

    1st

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3x1 font-bold">NextMall</h1>
+    </div>
+  )
+}
